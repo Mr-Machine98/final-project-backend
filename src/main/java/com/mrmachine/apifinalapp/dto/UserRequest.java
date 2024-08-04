234359ace6df3f8263bb73da7b05ff25f8cea19f
Juan Camilo Mamian Ruiz <mr.machineman98@gmail.com>
2024-08-04 12:26:08 -0500

    arreglando un bug

--- src/main/java/com/mrmachine/apifinalapp/dto/UserRequest.java
+++ src/main/java/com/mrmachine/apifinalapp/dto/UserRequest.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,5 +26,6 @@ public class UserRequest implements IUser {
 	@Column(unique = true)
 	private String email;
 	
+	@NotNull
 	private Boolean isAdmin;
 }
--- src/main/java/com/mrmachine/apifinalapp/models/auth/User.java
+++ src/main/java/com/mrmachine/apifinalapp/models/auth/User.java
@@ -17,6 +17,7 @@ import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -57,6 +58,7 @@ public class User implements IUser {
 	)
 	private List<Role> roles;
 	
+	@NotNull
 	@Transient
 	private Boolean isAdmin;
 	
